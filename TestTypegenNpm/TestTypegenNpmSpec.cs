67a3e0166b0ac493497d0195da3a5055c8e46f82
vivek <vivek.n@flyerssoft.com>
2023-03-17 12:44:30 +0530

    Adding typegen and projects.

---
+++ TestTypegenNpm/TestTypegenNpmSpec.cs
@@ -0,0 +1,12 @@
+﻿using TypeGen.Core.SpecGeneration;
+
+namespace TestTypegenNpm;
+
+public class TestTypegenNpmSpec: GenerationSpec
+{
+    public TestTypegenNpmSpec()
+    {
+        AddInterface<TestDto>();
+    }
+
+}
